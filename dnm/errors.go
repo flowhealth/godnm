95622c1c46e1bdd5a891fea473b2b0f0f083bb4d
Maciej Migacz <maciejmigacz@gmail.com>
2014-09-05 17:31:42 +0200

    add description to Error() - required to discover ProvisionedThroughputExceededException in canonical

--- dnm/errors.go
+++ dnm/errors.go
@@ -8,7 +8,7 @@ type TError struct {
 }
 
 func (self *TError) Error() string {
-	return fmt.Sprintf("DynamoDB Err: %s", self.Summary)
+	return fmt.Sprintf("DynamoDB Err: %s, desc: %s", self.Summary, self.Description)
 }
 
 func MakeError(summary, description string) *TError {
